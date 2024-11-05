21474c6a2dfe4e71c5e13e90c1b6825b0103ae29
COULOMB Paul <Paul.Coulomb@bordeaux-inp.fr>
2024-11-05 17:39:28 +0100

    avec tdd

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,8 +1,8 @@
 function passwordValidator(password) {
     const estDeLongueurMin = password.length >= 8;
-    const hasNumber = /\d/.test(password);
-    const hasUnelettre = /[a-zA-Z]/.test(password);
-    return estDeLongueurMin && hasNumber && hasUnelettre;
+    const aUnNumber = /\d/.test(password);
+    const aUnelettre = /[a-zA-Z]/.test(password);
+    return estDeLongueurMin && aUnNumber && aUnelettre;
 }
 
 module.exports = passwordValidator;
--- passwordValidator.spec.js
+++ passwordValidator.spec.js
@@ -1,17 +1,19 @@
 const isValidPassword = require('./passwordValidator');
 
 test('Le mdp est valide', () => {
-    expect(isValidPassword('12345678')).toBe(true);
-    expect(isValidPassword('1')).toBe(false);
+    const valid = isValidPassword('12345678')
+    expect(valid).toBe(true);
+    const invalid = isValidPassword('1')
+    expect(invalid).toBe(false);
 })
 
 test('Le mdp a au moins un chiffre', () => {
-    expect(isValidPassword('Leboncou1')).toBe(true);
-    expect(isValidPassword('LaBruteEtLeTruand')).toBe(false);
+    expect(isValidPassword('4bonarara')).toBe(true);
+    expect(isValidPassword('tomtometnana')).toBe(false);
 })
 
 
 test('Le mdp a au moins une lettre', () => {
-    expect(isValidPassword('1234567h')).toBe(true);
+    expect(isValidPassword('1234567x')).toBe(true);
     expect(isValidPassword('12345678')).toBe(false);
 })
