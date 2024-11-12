b3b1c9e66c4fff84fde3a7188f6dc9a9acdb7ff4
COULOMB Paul <Paul.Coulomb@bordeaux-inp.fr>
2024-11-12 17:06:16 +0100

    légère modif accountDAO.mjs

--- crud/accountDAO.mjs
+++ crud/accountDAO.mjs
@@ -5,21 +5,17 @@ export const accountDAO = {
     ACCOUNT_LIST.push(account);
   },
   retrieveAccountList() {
-    return ACCOUNT_LIST.map(({ id, lastName, firstName }) => ({
-        id,
-        lastName,
-        firstName
-      }));
+    return ACCOUNT_LIST.map(({ creationDate, ...rest }) => rest);
   },
   updateAccount(account) {
-    let index = ACCOUNT_LIST.findIndex((a) => a.id === account.id);
+    const index = ACCOUNT_LIST.findIndex((a) => a.id === account.id);
     if (index !== -1){
         ACCOUNT_LIST[index] = account;
     }
     console.log(ACCOUNT_LIST);
   },
   retrieveAccount(id) {
-    let copyAccount = ACCOUNT_LIST.find((account) => account.id === id);
+    const copyAccount = ACCOUNT_LIST.find((account) => account.id === id);
     return {
         id: copyAccount.id,
         name: copyAccount.lastName + " " + copyAccount.firstName,
